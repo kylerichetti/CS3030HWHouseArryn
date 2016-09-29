cda866fd57a634ac147056d292566a73f0f8a0a9
Gatleos <zfullmer9@hotmail.com>
2016-09-29 12:37:19 -0600

    Shell file checks for proper arguments

---
+++ arryn_hw3.sh
@@ -0,0 +1,35 @@
+
+me=`basename "$0"`
+
+help() { printf "Usage: $me"; exit 0; }
+
+# parse arguments
+awkFile=""
+sedFile=""
+inputFile=""
+
+while getopts "a:s:i:" o; do
+	case "${o}" in
+		a)
+			awkFile=${OPTARG}
+			;;
+		s)
+			sedFile=${OPTARG}
+			;;
+		i)
+			inputFile=${OPTARG}
+			;;
+		*)
+			echo "invalid arg"
+			help
+			;;
+	esac
+done
+
+# check that all arguments were given
+if [ -z "${awkFile}" ] || [ -z "${sedFile}" ]; || [ -z "${inputFile}" ] then
+	help
+fi
+
+
+exit 0
